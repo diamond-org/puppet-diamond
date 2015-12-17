b6c69b4d283a22edf3576877ddbbdda48d9f42e3
Ian Dennis Miller <ian@iandennismiller.com>
2015-12-17 18:49:10 -0500

    ensure compatibility with readthedocs

--- setup.py
+++ setup.py
@@ -1,11 +1,29 @@
+import os
+import re
 from setuptools import setup
 from distutils.dir_util import copy_tree
 
-import os
 
-version = '0.1'
+# from https://github.com/flask-admin/flask-admin/blob/master/setup.py
+def fpath(name):
+    return os.path.join(os.path.dirname(__file__), name)
 
-setup(version=version,
+
+def read(fname):
+    return open(fpath(fname)).read()
+
+
+file_text = read(fpath('__meta__.py'))
+
+
+def grep(attrname):
+    pattern = r"{0}\W*=\W*'([^']+)'".format(attrname)
+    strval, = re.findall(pattern, file_text)
+    return strval
+
+
+setup(
+    version=grep('__version__'),
     name='puppet_diamond',
     description="puppet_diamond",
     scripts=[
@@ -23,18 +41,11 @@ setup(version=version,
     classifiers=[],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
     include_package_data=True,
     keywords='',
-    author='Ian Dennis Miller',
-    author_email='iandennismiller@gmail.com',
-    url='http://www.iandennismiller.com',
-    install_requires=[
-        "mr.bob==0.1a9",
-        "distribute==0.7.3",
-        "Fabric==1.10.2",
-        "Sphinx==1.3.3",
-        "GitPython==1.0.1",
-        "alabaster==0.7.6",
-    ],
-    license='Proprietary',
+    author=grep('__author__'),
+    author_email=grep('__email__'),
+    url=grep('__url__'),
+    install_requires=read('requirements.txt'),
+    license='MIT',
     zip_safe=False,
 )
 
